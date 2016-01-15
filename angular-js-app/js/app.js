f8f258665488dcee36f941d470566d176ff791bc
Radu Matei <matei.radu94@gmail.com>
2016-01-16 01:34:10 +0200

    Modified app.js
    - added methods to remove and add products

--- angular-js-app/js/app.js
+++ angular-js-app/js/app.js
@@ -1,13 +1,45 @@
-var app = angular.module('blog', [ ]);
+var app = angular.module('angular-app', []);
 
-app.controller('HomeController', ['$scope', '$http', function($scope, $http) {
+app.controller('HomeController', ['$scope', '$http', function($scope, $http, $uibModal) {
   $scope.title = 'AWJ';
   $scope.subtitle = "Developing Java based web applications";
 }]);
 
 
 app.controller('ProductsController', ['$scope', '$http', function($scope, $http) {
+    
+    
+    $scope.id;
+    $scope.name;
+    $scope.category;
+    $scope.stock;
+    $scope.price;
+    
+    
     $http.get("http://localhost:8080/products").then(function(response){
      $scope.products = response.data;
   });
+  
+  $scope.removeProduct = function(id){
+    $http.delete("http://localhost:8080/products/delete/" + id).then(function(response){
+        
+        if(response.status != 200)
+            alert("Failed to delete product!");
+            
+        $("#product" + id).remove();
+    });  
+    
+    $scope.addProduct = function(){
+        $scope.productForm = {
+          id: $scope.id,
+          name: $scope.name,
+          category: $scope.category,
+          stock: $scope.stock,
+          price: $scope.price  
+        };
+        debugger;
+        $http.post("http://localhost:8080/products/create", $scope.productForm);
+    };
+
+  };
 }]);
