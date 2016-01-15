8dc952dcb5563aae69c42c6c30bd0cc63ebe5035
Radu Matei <matei.radu94@gmail.com>
2016-01-15 12:42:49 +0200

    Added request for /products

--- angular-js-app/js/app.js
+++ angular-js-app/js/app.js
@@ -1,6 +1,13 @@
 var app = angular.module('blog', [ ]);
 
-app.controller('HomeController', ['$scope', function($scope) {
-  $scope.helloWorld = 'Hello, Universe!';
+app.controller('HomeController', ['$scope', '$http', function($scope, $http) {
+  $scope.title = 'AWJ';
+  $scope.subtitle = "Developing Java based web applications";
+}]);
 
+
+app.controller('ProductsController', ['$scope', '$http', function($scope, $http) {
+    $http.get("http://localhost:8080/products").then(function(response){
+     $scope.products = response.data;
+  });
 }]);
