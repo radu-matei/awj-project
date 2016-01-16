2e3bd4b907df085574e13b316ff4e34fd4c2449b
Radu Matei <matei.radu94@gmail.com>
2016-01-16 03:00:41 +0200

    Added logic for edit in controller

--- angular-js-app/js/app.js
+++ angular-js-app/js/app.js
@@ -8,13 +8,18 @@ app.controller('HomeController', ['$scope', '$http', function($scope, $http, $ui
 
 app.controller('ProductsController', ['$scope', '$http', function($scope, $http) {
     
-    
+
     $scope.id;
     $scope.name;
     $scope.category;
     $scope.stock;
     $scope.price;
     
+    $scope.updateId;
+    $scope.updateName;
+    $scope.updateCategory;
+    $scope.updateStock;
+    $scope.updatePrice;
     
     $http.get("http://localhost:8080/products").then(function(response){
      $scope.products = response.data;
@@ -28,8 +33,9 @@ app.controller('ProductsController', ['$scope', '$http', function($scope, $http)
             
         $("#product" + id).remove();
     });  
-    
-    $scope.addProduct = function(){
+  };
+  
+  $scope.addProduct = function(){
         $scope.productForm = {
           id: $scope.id,
           name: $scope.name,
@@ -37,9 +43,25 @@ app.controller('ProductsController', ['$scope', '$http', function($scope, $http)
           stock: $scope.stock,
           price: $scope.price  
         };
-        debugger;
         $http.post("http://localhost:8080/products/create", $scope.productForm);
     };
-
-  };
+    
+    $scope.updateProduct = function(){
+        $scope.productForm = {
+          id: $scope.updateId,
+          name: $scope.updateName,
+          category: $scope.updateCategory,
+          stock: $scope.updateStock,
+          price: $scope.updatePrice  
+        };
+        $http.put("http://localhost:8080/products/update", $scope.productForm);
+    };
+    
+    $scope.getUpdateValues = function(product){
+        $scope.updateId = product.id;
+        $scope.updateName = product.name;
+        $scope.updateCategory = product.category;
+        $scope.updateStock = product.stock;
+        $scope.updatePrice = product.price;
+    }
 }]);
